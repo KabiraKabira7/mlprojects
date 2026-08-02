d20991db0525f38722109570001d9dc4e295ba7d
Tim Kabira <timothykabira@gmail.com>
2026-08-03 00:25:02 +0300

    updates

--- src/exception.py
+++ src/exception.py
@@ -7,13 +7,15 @@ def error_message_detail(error,error_details):
     error_message="Error occurred in python script name [{0}] line number [{1}]error message[{2}]".format(
     file_name,exc_tb.tb_lineno,str(error)
     
-    return error_message    
-        
     )
     
+    return error_message    
+        
+    
+    
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+    def __init__(self,error_message,error_detail):
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     
     def __str__(self):
@@ -23,6 +25,6 @@ class CustomException(Exception):
         
         try:
             a=1/0
-        except:
+        except Exception as e:
             logging.info("Divide by zero")
-            raise CustomException
+            raise CustomException(e,sys)
