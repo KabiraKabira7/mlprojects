45f858a594bd152eb811dd00b62055a44e850e4f
Tim Kabira <timothykabira@gmail.com>
2026-08-03 16:54:54 +0300

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,13 +9,13 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str=DS.path.join('artifacts',"train.csv")
+    train_data_path:str=os.path.join('artifacts',"train.csv")
     test_data_path:str=os.path.join('artifacts',"test.csv")
     raw_data_path:str=os.path.join('artifacts',"data.csv")
     
 class DataIngestion:
     def __init__(self):
-        self.ingestionconfigif=DataIngestionConfig()
+        self.ingestion_config=DataIngestionConfig()
         
     def initiate_data_ingestion(self):
         logging.info("Enter the data ingestion method or components")
@@ -23,7 +23,7 @@ class DataIngestion:
             df = pd.read_csv(r'C:\Users\PC\Desktop\ml_project\notebook\stud.csv')
             logging.info('Read the dataset as dataframe')
             
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist=ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             
@@ -39,9 +39,9 @@ class DataIngestion:
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
-        except:Exception as e:
+        except Exception as e:
             raise customException(e,sys)
         
 if __name__=="__main__":
     obj=DataIngestion()
-            pass
+    obj.initiate_data_ingestion()
--- src/logger.py
+++ src/logger.py
@@ -6,9 +6,9 @@ LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH=path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-logging.basicconfig(
+logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s ]%(lineno)d %(name)s - %(levelname)s -%(message)s",
     level=logging.INFO,
@@ -17,4 +17,4 @@ logging.basicconfig(
 
 
 if __name__=="__main__":
-    logging.info(logging has started)
+    logging.info("logging has started")
